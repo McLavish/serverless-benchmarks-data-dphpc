165a16270b5c43e89baffd997f37af8871c9ec7c
McLavish <Luca27ita@gmail.com>
2025-11-17 18:52:48 +0100

    fix: wrong extraction path of additional tar data

--- 600.workflows/download_datasets.sh
+++ 600.workflows/download_datasets.sh
@@ -16,10 +16,11 @@ mkdir -p 6100.1000-genome 6101.1000-genome-individuals 631.parallel-download 650
 
 tar -xzf "$ARCHIVE" \
   --directory . \
-  6100.1000-genome/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf \
-  6101.1000-genome-individuals/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf \
-  631.parallel-download/data-1048576-bytes.txt \
-  650.vid/frozen_inference_graph.pb \
-  660.map-reduce/words
+  --strip-components=2 \
+  benchmarks-data/600.workflows/6100.1000-genome/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf \
+  benchmarks-data/600.workflows/6101.1000-genome-individuals/ALL.chr21.phase3_shapeit2_mvncall_integrated_v5.20130502.sites.annotation.vcf \
+  benchmarks-data/600.workflows/631.parallel-download/data-1048576-bytes.txt \
+  benchmarks-data/600.workflows/650.vid/frozen_inference_graph.pb \
+  benchmarks-data/600.workflows/660.map-reduce/words
 
 echo "Datasets extracted under benchmarks-data/600.workflows"
